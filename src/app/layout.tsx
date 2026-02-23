b47670c17bce58b7efb7a937990a3af44450f1b3
saratendi69420-wq <sara.tendi69420@gmail.com>
2026-02-23 14:11:49 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,35 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${jost.variable} ${poppins.variable} antialiased`}
       >
+                {/* Meta Pixel Code */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '4130300227282627');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=4130300227282627&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
         <div className="fixed top-0 w-full z-50 backdrop-blur-sm">
           <MovingCards />
           <Navbar />
@@ -49,3 +78,4 @@ export default function RootLayout({
     </html>
   );
 }
+
